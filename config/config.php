f8c964d288bd8da1dd722f27feef7a193a4d7a70
BV Renova <110358231+LuizVazBr@users.noreply.github.com>
2022-07-31 21:18:16 -0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+/*Configuração*/
+class Cfg
+{  
+    //Entre -1 e 1 que aproxima o valor máximo esperado
+    const I = 0.293;
+    const D = -0.293;
+
+    //Aumento da classificação
+    const IN = 0.733;
+    const S =  -0.74;
+}
