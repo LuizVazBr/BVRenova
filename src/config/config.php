ab3707e0b46b278086a085ef78d658919a8c3048
BV Renova <110358231+LuizVazBr@users.noreply.github.com>
2022-07-31 21:25:08 -0300

    Create config.php

---
+++ src/config/config.php
@@ -0,0 +1,15 @@
+<?php
+/*Configuração*/
+class Cfg
+{  
+    //Entre -1 e 1 que aproxima o valor máximo esperado
+    const SIA_I = 0.293;
+    const SIA_D = -0.293;
+
+    //Aumento da classificação
+    const SIA_IN = 0.733;
+    const SIA_S =  -0.74;
+
+    //Níveis de pontuação
+    const SIA_N = 5;
+}
